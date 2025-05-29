d73168f870b571a85336563c372621f4acfacee7
1 <anton.shevelev.93@gmail.com>
2025-05-29 17:30:47 +0530

    Модуль Triangle вычисление периметра и площади

---
+++ sandbox/src/main/java/Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle {
+    public static void main(String[] args) {
+        var a = 5;
+        var b = 5;
+        var c = 7;
+        System.out.println("Площадь треугольника = " + (a+b+c));
+    }
+}
+
+
