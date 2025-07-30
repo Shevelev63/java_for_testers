b4df214c292ff831d82f558b2161b1b0277901d1
1 <anton.shevelev.93@gmail.com>
2025-07-30 18:37:26 +0530

    ДЗ 20

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -4,8 +4,13 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -18,66 +23,74 @@ public class ApplicationManager {
     private JdbcHelper jdbc;
     private HibernateHelper hbm;
 
-   public void init(String browser, Properties properties) {
+   public void init(String browser, Properties properties) throws MalformedURLException {
        this.properties = properties;
        if (driver == null) {
-            if ("firefox".equals(browser)){
-                driver = new FirefoxDriver();
-            } else if ("chrome".equals(browser)) {
-                driver = new ChromeDriver();
-                }
-            } else {
-            throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
-        }
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get(properties.getProperty("web.baseUrl"));
-            driver.manage().window().setSize(new Dimension(1936, 1056));
-            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
-        }
-
-
-    public GroupHelper groups() {
-       if (groups == null) {
-           groups = new GroupHelper(this);
+           var seleniumServer = properties.getProperty("selenuimServer");
+           if ("firefox".equals(browser)) {
+               if (seleniumServer != null) {
+                   driver = new RemoteWebDriver(new URL(seleniumServer), new FirefoxOptions());
+               } else {
+                   driver = new FirefoxDriver();
+               }
+               driver = new FirefoxDriver();
+           } else if ("chrome".equals(browser)) {
+               if (seleniumServer != null) {
+                   driver = new RemoteWebDriver(new URL(seleniumServer), new ChromeOptions());
+               } else {
+                   driver = new ChromeDriver();
+               }
+           } else {
+               throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+           }
+           Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+           driver.get(properties.getProperty("web.baseUrl"));
+           driver.manage().window().setSize(new Dimension(1936, 1056));
+           session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
        }
-       return groups;
-    }
+   }
 
-    public AddLoginHelper session() {
-        if (session == null){
-            session = new AddLoginHelper(this);
-        }
-        return session;
-    }
+       public GroupHelper groups () {
+           if (groups == null) {
+               groups = new GroupHelper(this);
+           }
+           return groups;
+       }
 
-    public ContactHelper contacts() {
-        if (contacts == null) {
-            contacts = new ContactHelper(this);
-        }
-        return contacts;
-    }
+       public AddLoginHelper session () {
+           if (session == null) {
+               session = new AddLoginHelper(this);
+           }
+           return session;
+       }
 
-    public boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
-    }
+       public ContactHelper contacts () {
+           if (contacts == null) {
+               contacts = new ContactHelper(this);
+           }
+           return contacts;
+       }
 
-    public JdbcHelper jdbc() {
-        if (jdbc == null) {
-            jdbc = new JdbcHelper(this);
-        }
-        return jdbc;
-    }
+       public boolean isElementPresent (By locator){
+           try {
+               driver.findElement(locator);
+               return true;
+           } catch (NoSuchElementException exception) {
+               return false;
+           }
+       }
 
-    public HibernateHelper hbm() {
-        if (hbm == null) {
-            hbm = new HibernateHelper(this);
-        }
-        return hbm;
-    }
+       public JdbcHelper jdbc () {
+           if (jdbc == null) {
+               jdbc = new JdbcHelper(this);
+           }
+           return jdbc;
+       }
 
-}
+       public HibernateHelper hbm () {
+           if (hbm == null) {
+               hbm = new HibernateHelper(this);
+           }
+           return hbm;
+       }
+   }
